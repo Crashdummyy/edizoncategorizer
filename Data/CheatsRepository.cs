03795bdd2eb18dfcd4af4d06c6108ad9115b8c14
Crashdummy <Simon.Schwendele@t-online.de>
2021-12-08 19:44:49 +0100

    initial

---
+++ Data/CheatsRepository.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EdizonCategorizer.Data
+{
+    public class CheatsRepository
+    {
+        public IEnumerable<CheatSection> DeserializeFrom(string path)
+        {
+            using var reader = File.OpenText(path);
+            
+            var results =new List<CheatSection> { new("UnCategorized", new List<(string name, string content)>()) };
+            
+            var currentLine = NextLine(reader);
+            while (!reader.EndOfStream && string.IsNullOrWhiteSpace(currentLine))
+                currentLine = NextLine(reader);
+
+            if (reader.EndOfStream)
+                return Enumerable.Empty<CheatSection>();
+            
+            var nextLine = NextLine(reader);
+            
+            while (!reader.EndOfStream)
+            {
+                var section = ReadOneSection(reader, ref currentLine, ref nextLine);
+                Action doStuff = section.Name.Equals("UnCategorized")
+                    ? () => results[0].Cheats.AddRange(section.Cheats)
+                    : () => results.Add(section);
+
+                doStuff();
+            }
+
+            return results;
+        }
+
+        private CheatSection ReadOneSection(StreamReader reader,
+                                            ref string currentLine,
+                                            ref string nextLine)
+        {
+            var result = new CheatSection();
+
+            (currentLine,nextLine) = TryAddSection(reader, currentLine, nextLine, result);
+
+            while (!reader.EndOfStream && !currentLine.StartsWith("[") || !nextLine.StartsWith("00000000"))
+                (currentLine, nextLine) = TryAddCheat(reader, currentLine, nextLine, result);
+
+
+            return result;
+        }
+
+        private (string, string) TryAddCheat(StreamReader reader,
+                                             string currentLine,
+                                             string nextLine,
+                                             CheatSection result)
+        {
+            if (!currentLine.StartsWith("[") || nextLine.StartsWith("00000000"))
+                return (currentLine, nextLine);
+            
+            var name = currentLine.Trim();
+            var code = nextLine.Trim();
+
+            currentLine = NextLine(reader);
+            while (!currentLine.StartsWith("["))
+            {
+                code += Environment.NewLine + currentLine.Trim();
+                currentLine = NextLine(reader);
+            }
+                
+            result.Cheats.Add((name,code));
+            return (currentLine, NextLine(reader));
+        }
+
+        private (string,string) TryAddSection(StreamReader reader, string currentLine, string nextLine, CheatSection result)
+        {
+            if (!currentLine.StartsWith("[") || !nextLine.StartsWith("00000000")) 
+                return (currentLine,nextLine);
+            
+            result.Name = currentLine.Trim();
+            currentLine = string.Empty;
+            while (string.IsNullOrWhiteSpace(currentLine))
+                currentLine = NextLine(reader);
+
+            return (currentLine, NextLine(reader));
+        }
+
+        private string NextLine(StreamReader reader) => reader.ReadLine()?.Trim() ?? string.Empty;
+    }
+    
+    public class CheatSection
+    {
+        public CheatSection()
+        {
+            Cheats = new List<(string name, string content)>();
+            Name = "UnCategorized";
+        }
+        
+        public CheatSection(string name, List<(string name, string content)> cheats)
+        {
+            Name = name;
+            Cheats = cheats;
+        }
+
+        public string Name { get; set; }
+        public List<(string name, string content)> Cheats { get;  }
+    }
+}
